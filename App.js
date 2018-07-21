e4ec51372af512a1e6f8161d6a05c14a7f1b3d99
damien <damien.savoldelli@gmail.com>
2018-07-21 19:12:59 +0200

    add task lists display

--- App.js
+++ App.js
@@ -1,6 +1,42 @@
 import React from 'react';
+import { View } from 'react-native';
+// import { Button as ButtonElement } from 'react-native-elements'; // alias
 import Header from './app/components/Header';
+import TaskList from './app/components/TaskList';
 
-export default () => (
-  <Header content="Liste des tâches" />
-);
+
+const list = [
+  {
+    id: 0,
+    content: 'Aller voir',
+    status: 'En cours',
+  },
+  {
+    id: 1,
+    content: 'Les dents',
+    status: 'En cours',
+  },
+  {
+    id: 2,
+    content: 'Ménage',
+    status: 'Terminé',
+  },
+];
+
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      list,
+    };
+  }
+
+  render() {
+    return (
+      <View>
+        <Header content="Liste des tâches" />
+        <TaskList taskList={this.state.list} />
+      </View>
+    );
+  }
+}
