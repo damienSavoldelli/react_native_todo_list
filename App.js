62c8050821d8428289f6ab293aafaaa30248517c
damien <damien.savoldelli@gmail.com>
2018-07-22 19:46:38 +0200

    update App, wrap navigator to reaload with translation

--- App.js
+++ App.js
@@ -1,7 +1,18 @@
 import React from 'react';
 
+import { translate } from 'react-i18next';
+
 import Navigator from './app/config/routes';
+import i18n from './app/config/i18n';
+
+// Wrapping a stack with translation hoc asserts we get new render on language change
+// the hoc is set to only trigger rerender on languageChanged
+const WrappedNavigator = ({ t }) => <Navigator screenProps={{ t }} onNavigationStateChange={null} />;
+const ReloadAppOnLanguageChange = translate('common', {
+  bindI18n: 'languageChanged',
+  bindStore: false,
+})(WrappedNavigator);
 
 export default () => (
-  <Navigator onNavigationStateChange={null} />
+  <ReloadAppOnLanguageChange />
 );
