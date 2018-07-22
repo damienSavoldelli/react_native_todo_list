22641ceb585a07c2833706dac1e70d9b1a885bcd
damien <damien.savoldelli@gmail.com>
2018-07-22 19:38:06 +0200

    add i18n config (dev true)

---
+++ app/config/i18n.js
@@ -0,0 +1,45 @@
+import i18n from 'i18next';
+import { reactI18nextModule } from 'react-i18next';
+import Expo from 'expo';
+
+import enLocals from '../locals/en.json';
+import frLocals from '../locals/fr.json';
+
+// creating a language detection plugin using expo
+// http://i18next.com/docs/ownplugin/#languagedetector
+const languageDetector = {
+  type: 'languageDetector',
+  async: true, // flags below detection to be async
+  detect: callback => Expo.DangerZone.Localization.getCurrentLocaleAsync().then((lng) => { callback(lng.replace('_', '-')); }),
+  init: () => {},
+  cacheUserLanguage: () => {},
+};
+
+i18n
+  .use(languageDetector)
+  .use(reactI18nextModule)
+  .init({
+    fallbackLng: 'en',
+
+    resources: {
+      en: enLocals,
+      fr: frLocals,
+    },
+
+    // have a common namespace used around the full app
+    ns: ['common'],
+    defaultNS: 'common',
+
+    debug: true,
+
+    // cache: {
+    //   enabled: true
+    // },
+
+    interpolation: {
+      escapeValue: false, // not needed for react as it does escape per default to prevent xss!
+    },
+  });
+
+
+export default i18n;
