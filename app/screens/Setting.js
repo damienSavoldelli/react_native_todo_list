99d817e3852daf45a57a75142524ef22847bb999
damien <damien.savoldelli@gmail.com>
2018-07-24 08:08:22 +0200

    update Setting screen, link with Translate screen

--- app/screens/Setting.js
+++ app/screens/Setting.js
@@ -1,6 +1,16 @@
 import React, { Component } from 'react';
-import { ScrollView, StatusBar, Text } from 'react-native';
+import { ScrollView, StatusBar } from 'react-native';
+import { List, ListItem } from 'react-native-elements';
 import { translate } from 'react-i18next';
+
+
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  list: {
+    marginTop: 0,
+  },
+});
 
 class Setting extends Component {
   constructor(props) {
@@ -13,13 +23,23 @@ class Setting extends Component {
     title: screenProps.t('setting:title')
   });
 
+  onTranslatePress = () => {
+    this.props.navigation.navigate('Translate');
+  };
+
   render() {
     const { t, i18n } = this.props;
 
     return (
       <ScrollView>
         <StatusBar translucent={false} barStyle="default" />
-        <Text>{t('setting:title')}</Text>
+        <List containerStyle={styles.list}>
+          <ListItem
+            key={1}
+            title={t('setting:language')}
+            onPress={() => this.onTranslatePress()}
+          />
+        </List>
       </ScrollView>
     );
   }
